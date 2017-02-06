cc9f0e12979477b788bc4062d973818a69a81143
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-02-06 16:08:05 +0500

    Added(rss-ladder): opportunity to redefine queue-prefix via env variable

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(*paths):
 
 setup(
     name='netutils-linux',
-    version='0.2.1',
+    version='0.2.2',
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
     license='MIT',
