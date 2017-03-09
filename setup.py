6c5ee8775bdacd505d1f5d5b7b401be6a12f8fbd
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-03-09 21:01:55 +0500

    Fixed: iterations error if disks/netdev dict is empty/None.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(*paths):
 
 setup(
     name='netutils-linux',
-    version='0.5.1',
+    version='0.5.3',
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
     license='MIT',
