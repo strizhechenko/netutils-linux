055678018ed5acb004d0a3cca7950f428577b1f7
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-03-23 14:28:20 +0500

    Added: more human readable output format in link rate (for both python 2.6 and 2.7+).

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(*paths):
 
 setup(
     name='netutils-linux',
-    version='1.0.1',
+    version='1.0.2',
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
     license='MIT',
