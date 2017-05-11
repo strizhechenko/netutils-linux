6f307a63d479571d27882c935ed543234c49d4d9
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-05-12 03:53:29 +0500

    Refactoring: detect vm script.

--- utils/detect_virt.py
+++ utils/detect_virt.py
@@ -1,86 +1,69 @@
 #!/usr/bin/env python
 
 import os
-import os.path
 
-dmi_vendor_table = ["KVM",  "QEMU", "VMware", "VMW",
-                    "innotek GmbH", "Xen", "Bochs", "Parallels", "BHYVE"]
+dmi_vendor_table = [
+    "KVM",
+    "QEMU",
+    "VMware",
+    "VMW",
+    "innotek GmbH",
+    "Xen",
+    "Bochs",
+    "Parallels",
+    "BHYVE"
+]
 
 
 def detect_vm_dmi():
     dmi_vendors = ['product_name', 'sys_vendor', 'board_vendor', 'bios_vendor']
-    sysfs_dmi_path = "/sys/class/dmi/id/"
-    for i in range(len(dmi_vendors)):
-        dmi_file = os.path.join(sysfs_dmi_path, dmi_vendors[i])
-        if obtainer(dmi_file):
-            element = reader(dmi_file)
-            if element in dmi_vendor_table:
-                print "Virtualization found in DMI:", element
-    return None
-
-
-def obtainer(path):
-    if os.path.exists(path):
-        return True
-    return False
-
-
-def reader(file):
-    try:
-        with open(file) as f:
-            return f.read().strip()
-    except IOError as e:
-        e.filename
-
-
-def detect_vm_xen():
-    xen_file = '/proc/xen/capabilitie'
-    if obtainer(xen_file):
-        print "Virtualization XEN found ({0} exists)".format(xen_file)
-    return None
+    for dmi_vendor in dmi_vendors:
+        dmi_file = os.path.join("/sys/class/dmi/id/", dmi_vendor)
+        if not os.path.exists(dmi_file):
+            continue
+        with open(dmi_file) as dmi_fd:
+            dmi_data = dmi_fd.read()
+            if dmi_data in dmi_vendor_table:
+                return dmi_data
 
 
 def detect_vm_hypervisor():
     hypervisor = "/sys/hypervisor/type"
-    if obtainer(hypervisor):
-        h = reader(hypervisor)
-        print "Virtualization found in /sys/hypervisor/type"
-        if h == 'xen':
-            print 'VIRTUALIZATION_XEN'
-        else:
-            print 'VIRTUALIZATION_VM_OTHER'
-    return None
+    if os.path.exists(hypervisor):
+        with open(hypervisor) as hypervisor_fd:
+            return hypervisor_fd.read()
 
 
 def detect_vm_uml():
-    proc = reader('/proc/cpuinfo')
-    if 'vendor_id\t: User Mode Linux' in proc:
-        print "UML virtualization found in /proc/cpuinfo"
-    return None
+    with open('/proc/cpuinfo') as cpuinfo:
+        if 'vendor_id\t: User Mode Linux' in cpuinfo.read():
+            return "UML"
 
 
 def detect_vm_zvm():
     zvm = '/proc/sysinfo'
-    if obtainer(zvm):
-        z = reader(zvm)
-        if 'z/VM' in z:
-            print 'VIRTUALIZATION_ZVM'
-        else:
-            print 'VIRTUALIZATION_KVM'
-    return None
+    if os.path.exists(zvm):
+        with open(zvm) as zvm_fd:
+            return "ZVM" if 'z/VM' in zvm_fd.read() else "KVM"
 
 
-def detect_vm_device_tree():
-    tree = '/proc/device-tree/hypervisor/compatible'
-    if obtainer(tree):
-        print 'VIRTUALIZATION_QEMU'
-    return None
+def detect_by_file(filename, vm):
+    if os.path.exists(filename):
+        return vm
 
+
+def main():
+    checks = (
+        detect_by_file('/proc/xen/capabilitie', "XEN"),
+        detect_by_file('/proc/device-tree/hypervisor/compatible', 'QEMU'),
+        detect_vm_zvm(),
+        detect_vm_uml(),
+        detect_vm_hypervisor(),
+        detect_vm_dmi()
+    )
+    if any(checks):
+        print checks
+        return 1
 
 if __name__ == '__main__':
-    detect_vm_dmi()
-    detect_vm_xen()
-    detect_vm_hypervisor()
-    detect_vm_device_tree()
-    detect_vm_uml()
-    detect_vm_zvm()
+    main()
