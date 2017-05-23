9c64c9819f641e2033f929b1e68d5ee1be80eb2b
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-05-24 00:18:58 +0500

    Refactoring: irqtop util has separated library now.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*paths):
 
 setuptools.setup(
     name='netutils-linux',
-    version='1.1.5',
+    version='1.1.8',
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
     license='MIT',
