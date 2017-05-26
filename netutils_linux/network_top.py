22df3e0a204a7c052ae6896d0a3a8b35709d1bff
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-05-26 13:35:43 +0500

    Some naive not working implementation of network-top

--- netutils_linux/network_top.py
+++ netutils_linux/network_top.py
@@ -9,15 +9,21 @@ from softnet_stat import SoftnetStatTop
 class NetworkTop(Top):
     def __init__(self):
         Top.__init__(self, None)
+        self.tops = {
+            'irqtop': IrqTop(),
+            'softnet_stat_top': SoftnetStatTop(),
+            'softirq_top': Softirqs(),
+        }
 
     def parse(self):
-        pass
+        return dict((top_name, top.parse()) for top_name, top in self.tops.iteritems())
 
     def eval(self):
-        pass
+        for top in self.tops.itervalues():
+            top.eval()
 
     def __repr__(self):
-        pass
+        return "\n".join(str(top) for top in self.tops)
 
 
 if __name__ == '__main__':
