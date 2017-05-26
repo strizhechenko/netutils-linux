bb77c271afca85fd3ee14a96822f1290758ba4f4
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-05-26 09:00:20 +0500

    Attempt to fix softirq-net-rx-top tests.

--- tests/softirq_net_rx_top_test.py
+++ tests/softirq_net_rx_top_test.py
@@ -9,7 +9,7 @@ class SoftirqsTest(unittest.TestCase):
     def test_file2data(self):
         for cpu in ('dualcore', 'i7'):
             for i in xrange(1, 6):
-                self.assertIn('NET_RX', Softirqs('tests/softirqs/{0}/softirqs{1}'.format(cpu, i)).file2data())
+                self.assertIn('NET_RX', Softirqs('tests/softirqs/{0}/softirqs{1}'.format(cpu, i)).parse())
 
 if __name__ == '__main__':
     unittest.main()
