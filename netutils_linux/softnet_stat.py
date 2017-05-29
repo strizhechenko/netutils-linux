114953ade56f18f64a209656e7d35cd53f67f00c
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-05-29 18:05:12 +0500

    Network top layout (#49)
    * Fixed: output layout for network-top looks well.
    * Fixed: tests for softnet-stat-top.
    * Changed: new __sub__() returns SoftnetStat object.
    * Added: __eq__() function, comparing each field in SoftnetStat objects.

--- netutils_linux/softnet_stat.py
+++ netutils_linux/softnet_stat.py
@@ -4,29 +4,51 @@ from base_top import BaseTop
 
 
 class SoftnetStat:
-    def __init__(self, row, cpu, random=False):
+    cpu = None
+    total = None
+    dropped = None
+    time_squeeze = None
+    cpu_collision = None
+    received_rps = None
+
+    def __init__(self, random=False):
+        self.random = random
+
+    def parse_string(self, row, cpu):
         row = [int('0x' + x, 16) for x in row.strip().split()]
         self.total, self.dropped, self.time_squeeze = row[0:3]
         self.cpu_collision = row[6]
         self.received_rps = row[7]
         self.cpu = cpu
-        self.random = random
+        return self
+
+    def parse_list(self, data):
+        self.cpu, self.total, self.dropped, self.time_squeeze, self.cpu_collision, self.received_rps = data
+        return self
 
     def __repr__(self):
         return "CPU: {0:2} total: {1:11} dropped: {2} time_squeeze: {3:4} cpu_collision: {4} received_rps: {5}" \
             .format(self.cpu, self.total, self.dropped, self.time_squeeze, self.cpu_collision, self.received_rps)
 
     def __sub__(self, other):
-        template = "CPU: {0:2} total: {1:8} dropped: {2} time_squeeze: {3} cpu_collision: {4} received_rps: {5}"
-        if self.random:
-            return template.format(self.cpu, randint(1, 100000), randint(0, 1), randint(0, 10), 0, randint(0, 5))
-        return template \
-            .format(self.cpu,
-                    self.total - other.total,
-                    self.dropped - other.dropped,
-                    self.time_squeeze - other.time_squeeze,
-                    self.cpu_collision - other.cpu_collision,
-                    self.received_rps - other.received_rps)
+        return SoftnetStat().parse_list([
+            self.cpu,
+            randint(1, 100000) if self.random else self.total - other.total,
+            randint(0, 1) if self.random else self.dropped - other.dropped,
+            randint(0, 10) if self.random else self.time_squeeze - other.time_squeeze,
+            0 if self.random else self.cpu_collision - other.cpu_collision,
+            randint(0, 5) if self.random else self.received_rps - other.received_rps
+        ])
+
+    def __eq__(self, other):
+        return all([
+            self.cpu == other.cpu,
+            self.total == other.total,
+            self.dropped == other.dropped,
+            self.time_squeeze == other.time_squeeze,
+            self.cpu_collision == other.cpu_collision,
+            self.received_rps == other.received_rps,
+        ])
 
 
 class SoftnetStatTop(BaseTop):
@@ -41,9 +63,10 @@ class SoftnetStatTop(BaseTop):
     def parse(self):
         with open(self.options.softnet_stat_file) as softnet_stat:
             data = softnet_stat.read().strip().split('\n')
-            return [SoftnetStat(row, cpu, self.options.random) for cpu, row in enumerate(data)]
+            return [SoftnetStat(self.options.random).parse_string(row, cpu) for cpu, row in enumerate(data)]
 
     def eval(self):
+        print self.current
         self.diff = [data - self.previous[cpu] for cpu, data in enumerate(self.current)]
 
     def __repr__(self):
