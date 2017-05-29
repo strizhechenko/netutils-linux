27f1f145734ace7f73895b16935b6a7844cb7c48
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-05-29 11:53:08 +0500

    Added: --random support in softnet-stat-top

--- netutils_linux/softnet_stat.py
+++ netutils_linux/softnet_stat.py
@@ -1,21 +1,26 @@
+from random import randint
 from optparse import Option
 from base_top import BaseTop
 
 
 class SoftnetStat:
-    def __init__(self, row, cpu):
+    def __init__(self, top, row, cpu):
         row = [int('0x' + x, 16) for x in row.strip().split()]
         self.total, self.dropped, self.time_squeeze = row[0:3]
         self.cpu_collision = row[6]
         self.received_rps = row[7]
         self.cpu = cpu
+        self.top = top
 
     def __repr__(self):
         return "CPU: {0:2} total: {1:11} dropped: {2} time_squeeze: {3:4} cpu_collision: {4} received_rps: {5}" \
             .format(self.cpu, self.total, self.dropped, self.time_squeeze, self.cpu_collision, self.received_rps)
 
     def __sub__(self, other):
-        return "CPU: {0:2} total: {1:8} dropped: {2} time_squeeze: {3} cpu_collision: {4} received_rps: {5}" \
+        template = "CPU: {0:2} total: {1:8} dropped: {2} time_squeeze: {3} cpu_collision: {4} received_rps: {5}"
+        if self.top.options.random:
+            return template.format(self.cpu, randint(1, 100000), randint(0, 1), randint(0, 10), 0, randint(0, 5))
+        return template \
             .format(self.cpu,
                     self.total - other.total,
                     self.dropped - other.dropped,
@@ -36,7 +41,7 @@ class SoftnetStatTop(BaseTop):
     def parse(self):
         with open(self.options.softnet_stat_file) as softnet_stat:
             data = softnet_stat.read().strip().split('\n')
-            return [SoftnetStat(row, cpu) for cpu, row in enumerate(data)]
+            return [SoftnetStat(self, row, cpu) for cpu, row in enumerate(data)]
 
     def eval(self):
         self.diff = [data - self.previous[cpu] for cpu, data in enumerate(self.current)]
