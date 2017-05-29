bcd32c9c124236e96e47f4541126614aaf177dc0
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-05-29 12:09:53 +0500

    Fixed: tests for softnet-stat-top

--- netutils_linux/softnet_stat.py
+++ netutils_linux/softnet_stat.py
@@ -4,13 +4,13 @@ from base_top import BaseTop
 
 
 class SoftnetStat:
-    def __init__(self, top, row, cpu):
+    def __init__(self, row, cpu, random=False):
         row = [int('0x' + x, 16) for x in row.strip().split()]
         self.total, self.dropped, self.time_squeeze = row[0:3]
         self.cpu_collision = row[6]
         self.received_rps = row[7]
         self.cpu = cpu
-        self.top = top
+        self.random = random
 
     def __repr__(self):
         return "CPU: {0:2} total: {1:11} dropped: {2} time_squeeze: {3:4} cpu_collision: {4} received_rps: {5}" \
@@ -18,7 +18,7 @@ class SoftnetStat:
 
     def __sub__(self, other):
         template = "CPU: {0:2} total: {1:8} dropped: {2} time_squeeze: {3} cpu_collision: {4} received_rps: {5}"
-        if self.top.options.random:
+        if self.random:
             return template.format(self.cpu, randint(1, 100000), randint(0, 1), randint(0, 10), 0, randint(0, 5))
         return template \
             .format(self.cpu,
@@ -41,7 +41,7 @@ class SoftnetStatTop(BaseTop):
     def parse(self):
         with open(self.options.softnet_stat_file) as softnet_stat:
             data = softnet_stat.read().strip().split('\n')
-            return [SoftnetStat(self, row, cpu) for cpu, row in enumerate(data)]
+            return [SoftnetStat(row, cpu, self.options.random) for cpu, row in enumerate(data)]
 
     def eval(self):
         self.diff = [data - self.previous[cpu] for cpu, data in enumerate(self.current)]
