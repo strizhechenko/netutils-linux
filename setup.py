8d51ae077439a5489874e639913f6b40ccf54046
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-06-05 15:10:13 +0500

    Version increment.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*paths):
 
 setuptools.setup(
     name='netutils-linux',
-    version='1.2.16',
+    version='1.2.18',
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
     license='MIT',
