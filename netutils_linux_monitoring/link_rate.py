d0fdce90886c80d38eb406730793973d40c6be26
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-06-07 22:36:03 +0500

    Refactoring: NUMA support + prettytable and colorama usage in link-rate.

--- netutils_linux_monitoring/link_rate.py
+++ netutils_linux_monitoring/link_rate.py
@@ -6,6 +6,8 @@ from optparse import Option
 from collections import namedtuple
 from netutils_linux_monitoring.base_top import BaseTop
 from netutils_linux_monitoring.layout import make_table
+from netutils_linux_monitoring.numa import Numa
+from netutils_linux_monitoring.colors import wrap, ColorsNode
 
 Stat = namedtuple('Stat', ['filename', 'shortname'])
 
@@ -29,7 +31,7 @@ class LinkRateTop(BaseTop):
     ]
     align_map = None
 
-    def __init__(self):
+    def __init__(self, numa=None):
         BaseTop.__init__(self)
         specific_options = [
             Option('--assert', '--assert-mode', default=False, dest='assert_mode',
@@ -47,6 +49,7 @@ class LinkRateTop(BaseTop):
             Option('--kbits', default=False, action='store_true'),
             Option('--mbits', default=True, action='store_true'),
         ]
+        self.numa = numa
         self.specific_options.extend(specific_options)
 
     def parse(self):
@@ -68,7 +71,10 @@ class LinkRateTop(BaseTop):
     def make_rows(self):
         repr_source = self.repr_source()
         for dev in self.options.devices:
-            yield [dev] + [repr_source[dev][stat] for stat in self.stats]
+            dev_node = self.numa.devices.get(dev)
+            dev_color = ColorsNode.get(dev_node)
+            _dev = wrap(dev, dev_color)
+            yield [_dev] + [repr_source[dev][stat] for stat in self.stats]
 
     def __repr__(self):
         return BaseTop.header + str(make_table(self.make_header(), self.align_map, list(self.make_rows())))
@@ -131,6 +137,8 @@ class LinkRateTop(BaseTop):
         self.unit_change()
         self.header = self.make_header()
         self.align_map = ['l'] + ['r'] * (len(self.header) - 1)
+        if not self.numa:
+            self.numa = Numa(self.options.devices, self.options.random)
 
     def unit_change(self):
         if not any([self.options.bits, self.options.kbits, self.options.mbits]):
