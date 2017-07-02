7ada8b4214028af11bcae4fbd0f03b4efb133d8b
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-07-02 15:27:57 +0500

    Refactoring: rss-ladder is rewriten in python

---
+++ netutils_linux_tuning/rss_ladder.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+
+""" Receive Side Scaling tuning utility """
+
+import re
+from os.path import join
+from argparse import ArgumentParser
+from six import iteritems, print_
+from netutils_linux_monitoring.numa import Numa
+from netutils_linux_hardware.assessor_math import any2int
+
+
+class RSSLadder(object):
+
+    def __init__(self):
+        interrupts_file = '/proc/interrupts'
+        self.options = self.parse_options()
+        lscpu_output = None
+        if self.options.test_dir:
+            interrupts_file = join(self.options.test_dir, "interrupts")
+            lscpu_output_filename = join(self.options.test_dir, "lscpu_output")
+            lscpu_output = open(lscpu_output_filename).read()
+            lscpu_output = bytes(lscpu_output, 'utf-8')  # compatibility with Popen.stdout
+        self.interrupts = open(interrupts_file).readlines()
+        self.numa = Numa(lscpu_output=lscpu_output)
+        for postfix in self.queue_postfixes_detect():
+            self.smp_affinity_list_apply(self.smp_affinity_list_make(postfix))
+
+    @staticmethod
+    def parse_options():
+        parser = ArgumentParser()
+        parser.add_argument('-t', '--test-dir', type=str,
+                            help="Use prepared test dataset in TEST_DIR directory instead of /proc/interrupts.")
+        parser.add_argument('-d', '--dry-run', help="Don't write anything to smp_affinity_list.",
+                            action='store_true', default=False)
+        parser.add_argument('-o', '--offset', type=int, default=0,
+                            help="If you have 2 NICs with 4 queues and 1 socket with 8 cpus, you may be want "
+                                 "distribution like this: eth0: [0, 1, 2, 3]; eth1: [4, 5, 6, 7]; "
+                                 "so run: rss-ladder-test eth0; rss-ladder-test --offset=4 eth1")
+        parser.add_argument('dev', type=str)
+        parser.add_argument('socket', nargs='?', type=int, default=0)
+        return parser.parse_args()
+
+    def queue_postfix_extract(self, line):
+        """
+        :param line: "31312 0 0 0 blabla eth0-TxRx-0"
+        :return: "-TxRx-"
+        """
+        queue_regex = r'{0}[^ \n]+'.format(self.options.dev)
+        queue_name = re.findall(queue_regex, line)
+        if queue_name:
+            return re.sub(r'({0}|[0-9])'.format(self.options.dev), '', queue_name[0])
+
+    def queue_postfixes_detect(self):
+        """
+        self.dev: eth0
+        :return: "-TxRx-"
+        """
+        return set([line for line in [self.queue_postfix_extract(line) for line in self.interrupts] if line])
+
+    def smp_affinity_list_make(self, postfix):
+        """
+        :param postfix: "-TxRx-"
+        :return: list of tuples(irq, queue_name, socket)
+        """
+        print_("- distribute interrupts of {0} ({1}) on socket {2}".format(
+            self.options.dev, postfix, self.options.socket))
+        queue_regex = r'{0}{1}[^ \n]+'.format(self.options.dev, postfix)
+        socket_cpus = [k for k, v in iteritems(self.numa.socket_layout) if v == self.options.socket] * 4
+        socket_cpus.reverse()
+        for line in self.interrupts:
+            queue_name = re.findall(queue_regex, line)
+            if queue_name:
+                yield any2int(line.split()[0]), queue_name[0], socket_cpus.pop()
+
+    def smp_affinity_list_apply(self, smp_affinity):
+        """
+        '* 4' is in case of NIC has more queues than socket has CPUs
+        :param smp_affinity: list of tuples(irq, queue_name, socket)
+        """
+        for irq, queue_name, socket_cpu in smp_affinity:
+            print_("  - {0}: irq {1} {2} -> {3}".format(self.options.dev, irq, queue_name, socket_cpu))
+            if self.options.dry_run:
+                continue
+            filename = "/proc/irq/{0}/smp_affinity_list".format(irq)
+            with open(filename, 'w') as irq_file:
+                irq_file.write(str(socket_cpu))
+
+
+if __name__ == '__main__':
+    RSSLadder()
