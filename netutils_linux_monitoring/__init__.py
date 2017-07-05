4c8b951ad1f075f1e6b10028a247b551fe45cb31
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-07-05 23:11:20 +0500

    Fixed: import error with network-top.

--- netutils_linux_monitoring/__init__.py
+++ netutils_linux_monitoring/__init__.py
@@ -1,7 +1,12 @@
+# coding: utf-8
+
+""" Top-like utils for the linux networking stack """
+
 from netutils_linux_monitoring.irqtop import IrqTop
 from netutils_linux_monitoring.link_rate import LinkRateTop
-from netutils_linux_monitoring.network_top import NetworkTop
 from netutils_linux_monitoring.softirqs import Softirqs
 from netutils_linux_monitoring.softnet_stat import SoftnetStatTop
+from netutils_linux_monitoring.network_top import NetworkTop
+
 
 __all__ = ['SoftnetStatTop', 'IrqTop', 'Softirqs', 'NetworkTop', 'LinkRateTop']
