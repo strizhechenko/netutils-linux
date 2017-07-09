50cc3c7767a7494981dc6ea921adc3ce64c6f174
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-07-09 13:08:28 +0500

    Fixed: rss-ladder test setup is dry-run now.

--- netutils_linux_tuning/rss_ladder_test.py
+++ netutils_linux_tuning/rss_ladder_test.py
@@ -7,8 +7,7 @@ from netutils_linux_tuning.rss_ladder import RSSLadder
 class RSSLadderTests(TestCase):
 
     def setUp(self):
-        self.rss = RSSLadder(argv=['eth0'])
-        self.rss.options.dev = 'eth0'
+        self.rss = RSSLadder(argv=['--dry-run', 'eth0'])
 
     def test_queue_postfix_extract(self):
         line = "31312 0 0 0 blabla eth0-TxRx-0"
