9cc8388a4d07331168409c933ddd719684655c40
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-07-11 02:23:13 +0500

    Refactoring: simplified AutoRPSTests.

--- netutils_linux_tuning/autorps_test.py
+++ netutils_linux_tuning/autorps_test.py
@@ -6,17 +6,11 @@ from netutils_linux_tuning.autorps import AutoRPS
 
 
 class AutoRPSTests(TestCase):
-    def test_cpus2mask_first_socket(self):
-        expected = 'f0'
-        cpus = [0, 1, 2, 3]
-        self.assertEqual(AutoRPS.cpus2mask(cpus, 8), expected)
-
-    def test_cpus2mask_second_socket(self):
-        expected = 'f'
-        cpus = [4, 5, 6, 7]
-        self.assertEqual(AutoRPS.cpus2mask(cpus, 8), expected)
-
-    def test_cpus2mask_all(self):
-        expected = 'ff'
-        cpus = list(range(8))
-        self.assertEqual(AutoRPS.cpus2mask(cpus, 8), expected)
+    def test_all(self):
+        dataset = {
+            'f0': list(range(4)),
+            'f': list(range(4, 8)),
+            'ff': list(range(8))
+        }
+        for expected, cpus in dataset.items():
+            self.assertEqual(AutoRPS.cpus2mask(cpus, 8), expected)
