5194e1e9491210f0b2a0ba4ac62be68c818f91cc
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2018-01-16 01:57:06 +0500

    Refactoring(server-info): moved last subsystem (system) to the separate class
    Got rid from unused imports in rater
    Mapped subsystem classes to their keys and got rid from that long if elif elif elif elif

--- netutils_linux_hardware/rater.py
+++ netutils_linux_hardware/rater.py
@@ -5,49 +5,36 @@ import yaml
 from netutils_linux_hardware.cpu import CPU
 from netutils_linux_hardware.disk import Disk
 from netutils_linux_hardware.memory import Memory
-from netutils_linux_hardware.folding import Folding
-from netutils_linux_hardware.grade import Grade
+from netutils_linux_hardware.system import System
 from netutils_linux_hardware.net import Net
-from netutils_linux_hardware.rater_math import extract
+from netutils_linux_hardware.folding import Folding
 
 
 class Rater(object):
     """ Calculates rates for important system components """
-    info = None
-    avg = None
-    keys = ('net', 'cpu', 'memory', 'system', 'disk')
+    subsystems = {
+        'net': Net,
+        'cpu': CPU,
+        'system': System,
+        'memory': Memory,
+        'disk': Disk,
+    }
 
     def __init__(self, data, args):
         self.data = data
         self.args = args
         self.folding = Folding(args)
+        self.info = None
         if self.data:
             self.rate()
 
-    def __str__(self):
-        return yaml.dump(self.info, default_flow_style=False).strip()
-
     def rate(self):
+        """ Rate every subsystem of the server """
         data = dict()
-        for key in self.keys:
-            if not getattr(self.args, key):
-                continue
-            elif key == 'net':
-                data[key] = Net(self.data, self.folding).rate()
-            elif key == 'cpu':
-                data[key] = CPU(self.data, self.folding).rate()
-            elif key == 'memory':
-                data[key] = Memory(self.data, self.folding).rate()
-            elif key == 'disk':
-                data[key] = Disk(self.data, self.folding).rate()
-            elif key == 'system':
-                data[key] = self.rate_system()
+        for key, subsystem in self.subsystems.items():
+            if getattr(self.args, key):
+                data[key] = subsystem(self.data, self.folding).rate()
         self.info = self.folding.fold(data, Folding.SERVER)
 
-    def rate_system(self):
-        cpuinfo = extract(self.data, ['cpu', 'info'])
-        if cpuinfo:
-            return self.folding.fold({
-                'Hypervisor vendor': Grade.fact(cpuinfo.get('Hypervisor vendor'), False),
-                'Virtualization type': Grade.fact(cpuinfo.get('Hypervisor vendor'), False),
-            }, Folding.SUBSYSTEM)
+    def __str__(self):
+        return yaml.dump(self.info, default_flow_style=False).strip()
