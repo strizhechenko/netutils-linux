54de1cb9ccbb765d859bd8c3f697d30e98cb1dd4
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2018-10-28 22:16:27 +0500

    Added(setup): python3.7 official support.

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setuptools.setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development',
         'Topic :: Utilities',
     ],
