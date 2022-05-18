f54359ce31e061350993a201be69524fc7a8fec0
Oleg Strizhechenko <oleg@carbonsoft.ru>
2022-05-18 18:21:47 +0500

    Dependencies: removed frozen version of colorama and pyyaml. Splitted dev deps.

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development',
         'Topic :: Utilities',
     ],
